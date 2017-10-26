84ee8c5bbc067fd4b910e5911db961ab75d2a041
sf4e <Revolution9>
2017-10-26 21:46:34 +0900

    Initial commit

---
+++ app/src/main/java/eugenio/nuc/JuicerControl.java
@@ -0,0 +1,13 @@
+package eugenio.nuc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JuicerControl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_juicer_control);
+    }
+}
---
+++ app/src/main/java/eugenio/nuc/nuc_health.java
@@ -0,0 +1,13 @@
+package eugenio.nuc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class nuc_health extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuc_health);
+    }
+}
